9315d90235909c774531edcbedb39ae829456711
Ajay Nanda <ajnanda@gmail.com>
2022-03-13 14:01:20 -0700

    draft of interpolate method

--- matlab/HybridArc.m
+++ matlab/HybridArc.m
@@ -149,49 +149,67 @@ classdef HybridArc
             this.plotByFnc('plotPhase', varargin{:})
         end
 
-        function splitTimes(this) //takes this.t and splits into sections based on this.jump_times
-          this.times_per_jump = [];
-          start_row = 1;
-          for jump_idx = 1:length(this.jump_times) //jump index
-            column = 1;
-            for time_idx = start_row:length(this.t) //time index
-              if this.t(time_idx,1) < this.jump_times(jump_idx,1) // if time less than jump point
-                this.times_per_jump(jump_idx, column) = this.t(time_idx, 1); //add time to section of data before that jump
-                column = column + 1;
-                start_row = start_row + 1
-              else
-                break
-          //add times after last jump
-          last_col = 1;
-          last_row = length(this.jump_times) + 1;
-          while start_row <= length(this.t)
-            this.times_per_jump(last_row, last_col) = this.t(start_row, 1);
-            start_row = start_row + 1;
-            last_col = last_col + 1;
-        end
-
-        function decimate(this)
-          this.decimated_times = [];
-          this.decimated_x = [];
-          for row = 1:size(this.times_per_jump, 1)
-            r = 10 //TODO: find better reduction factor, possibly fomula
-            x = this.times_per_jump(row, :);
-            decimated_times(row, :) = decimate(x, r);
-
-          col = 1;
-          for row = 1:size(this.decimated_times, 1)
-            for col = 1:size(this.decimated_times, 2)
-              this.decimated_x(row, )
-
-        end
+        // function splitTimes(this) //takes this.t and splits into sections based on this.jump_times
+        //   this.times_per_jump = [];
+        //   start_row = 1;
+        //   for jump_idx = 1:length(this.jump_times) //jump index
+        //     column = 1;
+        //     for time_idx = start_row:length(this.t) //time index
+        //       if this.t(time_idx,1) < this.jump_times(jump_idx,1) // if time less than jump point
+        //         this.times_per_jump(jump_idx, column) = this.t(time_idx, 1); //add time to section of data before that jump
+        //         column = column + 1;
+        //         start_row = start_row + 1
+        //       else
+        //         break
+        //   //add times after last jump
+        //   last_col = 1;
+        //   last_row = length(this.jump_times) + 1;
+        //   while start_row <= length(this.t)
+        //     this.times_per_jump(last_row, last_col) = this.t(start_row, 1);
+        //     start_row = start_row + 1;
+        //     last_col = last_col + 1;
+        // end
+        //
+        // function decimate(this)
+        //   this.decimated_times = [];
+        //   this.decimated_x = [];
+        //   for row = 1:size(this.times_per_jump, 1)
+        //     r = 10 //TODO: find better reduction factor, possibly fomula
+        //     x = this.times_per_jump(row, :);
+        //     decimated_times(row, :) = decimate(x, r);
+        //
+        //   col = 1;
+        //   for row = 1:size(this.decimated_times, 1)
+        //     for col = 1:size(this.decimated_times, 2)
+        //       this.decimated_x(row, )
+        //
+        // end
+        //
+        // function interpolate(this)
+        //   this.splitTimes();
+        //   this.decimate();
+        //   for row = 1:size(this.decimated_times, 1)
+        //     xq = this.decimated_times;
+        //     V = interp1(, this.x)
+        // end
 
         function interpolate(this)
-          this.splitTimes();
-          this.decimate();
-          for row = 1:size(this.decimated_times, 1)
-            xq = this.decimated_times;
-            V = interp1(, this.x)
+          interpolated_x = []
+          for jump = 0: size(this.jump_times)
+            t_at_jump = t(:, j == jump)
+            x_at_jump = x(:, j == jump)
+            t_grid_at_jump = t_grid((t_grid >= t_at_jump(1,1)) & (t_grid <= t_at_jump(size(t), 1)))
+            interp_x_at_jump = interp1(t_at_jump, x_at_jump, t_grid_at_jump)
+
+            interpolated_x = [interpolated_x, interp_x_at_jump]
+
+          if t_grid >= t_at_jump(size(t), 1)
+            interp_rest_of_t = interp1(t_at_jump, x_at_jump, t_grid)  //not sure if this is how to do rest of t grid or not
+
+
+          end
         end
+
 
     end
 
